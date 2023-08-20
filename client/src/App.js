664fdd5d3b93d83f298638d3df14d7e60c95ecdf
Chris0217 <102713143+Chris0217@users.noreply.github.com>
2023-08-20 22:23:15 +0700

    set routers in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Carousel from "./components/Carousel";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ProductDetail from "./components/ProductDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <div className="App">
+          <Routes>
+            <Route exact path="/" element={<Carousel />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/detail" element={<ProductDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
