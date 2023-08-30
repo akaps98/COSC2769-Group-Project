6700c3ce8779299a35015d2c37e094ecf77930b4
duynguyen-workspace <duynguyen.workspace@gmail.com>
2023-08-30 08:40:15 +0700

    update carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,15 @@ import searchIcon from "../assets/images/search-icon.svg"
 import shoppingCartIcon from "../assets/images/shopping-cart-icon.png"
 import banner from "../assets/images/sale-banner-header.png"
 
+// Header Icons
+import icon1 from "../assets/images/header-icon-1.png"
+import icon2 from "../assets/images/header-icon-2.png"
+import icon3 from "../assets/images/header-icon-3.png"
+import icon4 from "../assets/images/header-icon-4.png"
+
 function Header() {
     return (
-        <header className="header">
+        <header className="header pb-3">
             <div className="headerTop">
                 <nav>
                     <ul className="">
@@ -30,6 +36,32 @@ function Header() {
                     <div className="banner">
                         <img src={banner}></img>
                     </div>
+                </div>
+            </div>
+            <div className="headerCaption d-flex justify-content-center align-items-center">
+                <select name="cars" id="cars">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="opel">Opel</option>
+                    <option value="audi">Audi</option>
+                </select>
+                <div>
+                    <a href="#">
+                        <img src={icon1} className="d-inline-block"></img>
+                        <span>LazMall</span>
+                    </a>
+                    <a href="#">
+                        <img src={icon2} className="d-inline-block"></img>
+                        <span>Vouchers</span>
+                    </a>
+                    <a href="#">
+                        <img src={icon3} className="d-inline-block"></img>
+                        <span>Top Up & eCoupon</span>
+                    </a>
+                    <a href="#">
+                        <img src={icon4} className="d-inline-block"></img>
+                        <span>LazGlobal</span>
+                    </a>
                 </div>
             </div>
         </header>
